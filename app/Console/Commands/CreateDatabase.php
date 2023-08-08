5d08066d10503334662e032170b0634f1be4f97c
ibrahimmalii <eng.ibrahimmalii@gmail.com>
2023-08-09 01:43:22 +0300

    create command for create database automatically.

---
+++ app/Console/Commands/CreateDatabase.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
+
+class CreateDatabase extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'db:create {database?}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new database';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): void
+    {
+        $databaseName = $this->argument('database') ?: config('database.connections.' . config('database.default') . '.database');
+
+        $validator = Validator::make(['database' => $databaseName], [
+            'database' => 'required|string|max:64',
+        ]);
+
+        if ($validator->fails()) {
+            $this->error('Invalid database name provided.');
+            return;
+        }
+
+        $query = "CREATE DATABASE IF NOT EXISTS $databaseName CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;";
+        DB::statement($query);
+
+        $this->info("Database $databaseName created successfully.");
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\CreateDatabase;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -24,4 +25,9 @@ class Kernel extends ConsoleKernel
 
         require base_path('routes/console.php');
     }
+
+    protected $commands = [
+        CreateDatabase::class,
+    ];
+
 }
