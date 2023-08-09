b94bd4736c9c2bc2407622be7ba280a646fd6e04
ibrahimmalii <eng.ibrahimmalii@gmail.com>
2023-08-09 14:13:38 +0300

    Complete test cases and do some enhancements.

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -2,7 +2,9 @@
 
 namespace Database\Factories;
 
+use App\Http\Enums\RoleEnums;
 use App\Models\Task;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -20,8 +22,12 @@ class TaskFactory extends Factory
         return [
             'title' => fake()->sentence(),
             'description' => fake()->paragraph(),
-            'assigned_to_id' => fake()->numberBetween(1, 1000),
-            'assigned_by_id' => fake()->numberBetween(1000, 1100),
+            'assigned_to_id' => function () {
+                return User::factory()->create(['role_id' => RoleEnums::User->value])->id;
+            },
+            'assigned_by_id' => function () {
+                return User::factory()->create(['role_id' => RoleEnums::Admin->value])->id;
+            }
         ];
     }
 }
--- tests/Feature/StatisticTest.php
+++ tests/Feature/StatisticTest.php
@@ -7,20 +7,26 @@ use App\Models\Role;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Support\Facades\DB;
 use Tests\TestCase;
 
 class StatisticTest extends TestCase
 {
     use RefreshDatabase, WithoutMiddleware;
 
-    public function test_update_statistics_job_when_create_new_task(): void
+    public function setUp(): void
     {
+        parent::setUp();
+
         Role::factory()->count(2)->create();
-        $admin = User::factory()->create([
+        $this->admin = User::factory()->create([
             'role_id' => RoleEnums::Admin->value,
         ]);
-
-        $this->actingAs($admin);
+        DB::statement('ALTER TABLE roles AUTO_INCREMENT = 1;');
+    }
+    public function test_update_statistics_job_when_create_new_task(): void
+    {
+        $this->actingAs($this->admin);
         $response = $this->get('/statistics');
 
         $response->assertStatus(200);
