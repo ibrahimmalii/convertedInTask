2ea5c5f8a1a390d5abb57f11722ad27178be936e
ibrahimmalii <eng.ibrahimmalii@gmail.com>
2023-08-09 14:24:18 +0300

    enhancement in UserSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 use Database\Factories\UserFactory;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class UserSeeder extends Seeder
 {
@@ -13,7 +15,20 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory()->count(1000)->create();
+        $values = [];
+
+        for ($i = 0; $i < 10000; $i++) {
+            $values[] = [
+                'name' => fake()->name(),
+                'email' => fake()->unique()->safeEmail(),
+                'email_verified_at' => now(),
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+                'role_id' => 2,
+                'remember_token' => Str::random(10),
+            ];
+        }
+
+        User::insert($values);
 
         User::factory()->count(100)->create([
             'role_id' => 1,
